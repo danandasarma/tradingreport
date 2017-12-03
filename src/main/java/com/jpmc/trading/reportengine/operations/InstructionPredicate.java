9d1c370df810def3f6e150a7ff351ab470aceaa0
Ananda Sarma Damaraju <adamaraju@innominds.com>
2017-12-03 10:17:36 +0530

    Added logic for retrieving the incoming trade amount based on settlement date

---
+++ src/main/java/com/jpmc/trading/reportengine/operations/InstructionPredicate.java
@@ -0,0 +1,12 @@
+package com.jpmc.trading.reportengine.operations;
+
+import java.time.LocalDate;
+import java.util.function.Predicate;
+
+import com.jpmc.trading.reportengine.dto.Instruction;
+
+public class InstructionPredicate {
+	public static Predicate<Instruction> isForSettlement(LocalDate currentDate) {
+		return instruction -> instruction.getSettlementDate().equals(currentDate);
+	}
+}
--- src/main/java/com/jpmc/trading/reportengine/service/ReportEngineService.java
+++ src/main/java/com/jpmc/trading/reportengine/service/ReportEngineService.java
@@ -1,14 +1,19 @@
 package com.jpmc.trading.reportengine.service;
 
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import com.jpmc.trading.reportengine.dto.Instruction;
 import com.jpmc.trading.reportengine.exception.ApplicationException;
+import com.jpmc.trading.reportengine.operations.InstructionPredicate;
 import com.jpmc.trading.reportengine.parser.IDataParser;
 import com.jpmc.trading.reportengine.parser.InstructionCSVParser;
 
@@ -38,6 +43,7 @@ public class ReportEngineService {
 	 */
 	public void getIncomingInstructions() throws ApplicationException {
 
+		Map<LocalDate, Double> dailyTradeAmountsMap = new LinkedHashMap<>();
 		System.out.println("Parsing incoming instructions...");
 		instructionParser = new InstructionCSVParser();
 
@@ -52,6 +58,26 @@ public class ReportEngineService {
 		System.out.println("Incoming instructions:");
 
 		incomingInstructions.forEach(instruction -> System.out.println(instruction));
+
+		for (int day = 1; day <= 30; day++) {
+			Double tradeAmount = 0d;
+			LocalDate currentDate = LocalDate.of(2017, Month.NOVEMBER, day);
+			List<Instruction> settlementDateList = incomingInstructions.stream()
+					.filter(InstructionPredicate.isForSettlement(currentDate)).collect(Collectors.toList());
+
+			for (Instruction instruction : settlementDateList) {
+				Double settlementAmount = instruction.getPricePerUnit() * instruction.getUnits()
+						* instruction.getExchangeRate();
+
+				tradeAmount += settlementAmount;
+			}
+			dailyTradeAmountsMap.put(currentDate, tradeAmount);
+		}
+
+		NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
+
+		dailyTradeAmountsMap.entrySet().stream().forEach(instruction -> System.out.println(
+				"Trade amount on " + instruction.getKey() + " is " + currencyFormatter.format(instruction.getValue())));
 	}
 
 	/**
